78c36ce7428e34c32065904c50b8701315483244
Willi Lazarov <lazarov.w@gmail.com>
2020-11-01 20:48:48 +0100

    Establish database connection

--- db.php
+++ db.php
@@ -1,1 +1,13 @@
 <?php
+$dbHost = "hosting.midgard.cz";
+$dbUsername = "wlazarov_vut";
+$dbPassword = "IV1Q1fJ7JP";
+
+// Create connection
+$conn = new mysqli($dbHost, $dbUsername, $dbPassword);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
